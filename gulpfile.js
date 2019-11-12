415d972cc8d5ed277affc1d76036aedd48f13cc0
flamingPoncho <flameofsandra@gmail.com>
2019-11-12 16:22:36 +0300

    template

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const browserSync = require('browser-sync')
+const uglify = require('gulp-uglify')
+
+gulp.task('sass', function() {
+    return (
+        gulp.src('./app/scss/**/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(gulp.dest('./app/css'))
+        .pipe(browserSync.reload({stream: true}))
+    )
+})
+
+gulp.task('html', function() {
+    return (
+        gulp.src('./app/*.html')
+        .pipe(browserSync.reload({stream: true}))
+    )
+})
+
+gulp.task('js', function() {
+    return (
+        gulp.src('./app/js-raw/**/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./app/js-dist'))
+        .pipe(browserSync.reload({stream: true}))
+    )
+})
+
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./app/"
+        }
+    });
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./app/scss/**/*.scss', gulp.parallel('sass'))
+    gulp.watch('./app/*.html', gulp.parallel('html'))
+    gulp.watch('./app/js-raw/**/*.js', gulp.parallel('js'))
+})
+
+gulp.task('default', gulp.parallel('browser-sync', 'watch'))
